287545101f5a262538fce0fca9987df8cce6ed2a
vine_dasha_ <56861606+vinedasha@users.noreply.github.com>
2022-01-31 00:39:30 +0300

    Delete 5.sql

--- 5.sql
+++
@@ -1,6 +0,0 @@
-select count(distinct id_bliznec)
-from deti_roditeli
-where id_children in  (select id_people
-from families
-where id_people in (select id_people
-from family_obqed)) and id_bliznec is not null;
