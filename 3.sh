4bb757a0f06f5ac866694d72f35e5d549ade2607
vine_dasha_ <56861606+vinedasha@users.noreply.github.com>
2022-01-31 00:39:05 +0300

    Delete 3.sh

--- 3.sh
+++
@@ -1,2 +0,0 @@
-cat /var/www/localhost/htdocs/sh/3.sql | ssh postgres@172.1.0.3 ‘/usr/local/pqsql/bin/psql –d lab03_var01’ 
-cat /var/www/localhost/htdocs/sh/3.5.sql | ssh postgres@172.1.0.3 ‘/usr/local/pqsql/bin/psql –d lab03_var01’ 
